0b411609012a9e89faeb96dc0135810b2a9da2f0
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2018-05-18 21:30:09 -0700

    React-router setup

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>
+                    Maily!
+                </h1>
+                Collect feedbak from your users
+            </div>
+        )
+    }
+}
+
+export default Landing;
