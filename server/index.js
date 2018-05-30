5aa0e71080c1735e593a78a763556529fd5906e6
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2018-05-29 23:40:13 -0700

    Stripe API in progress

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +10,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30*24*60*60*1000,
@@ -19,6 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
