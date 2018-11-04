6ae5c34025acb7993236547923a92d0cf0763436
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2018-11-04 13:10:02 -0500

    Working on  user responses

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,13 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
     app.post('/api/survey/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
-        const events = _.chain(req.body)
+
+        _.chain(req.body)
         .map(({ email, url}) => {
             const match = p.test(new URL(url).pathname);
             if(match) {
@@ -25,8 +26,22 @@ module.exports = app => {
         })
         .compact()
         .uniqBy('email', 'surveyId')
+        .each(({ surveyId, email, choice}) => {
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email }
+                }
+            }, {
+                $inc: { [choice] : 1 },
+                $set: { 'recipients.$.responded': true},
+                lastResponded: new Date()
+            })
+            .then(result => console.log(result)); 
+        })
         .value();
-        console.log(events);
+        
+        res.send({});
     });
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients} = req.body;
@@ -35,7 +50,7 @@ module.exports = app => {
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({ email:email.trim() })),
+            recipients: recipients.split(',').map(email => ({ email:email.trim(), responded: false })),
             _user: req.user.id,
             dateSent: Date.now()
         });
